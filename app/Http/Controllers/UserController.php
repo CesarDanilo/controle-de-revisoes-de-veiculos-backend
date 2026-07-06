a31fa274085846c4618a5a18aaad5ba164770fe9
Cesar <billypalacios616@gmail.com>
2026-07-06 13:13:00 -0400

    feat: primeiro commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $current_page = $request->query('current_page') ?? 1;
+        $per_page = 10;
+        $skip = ($current_page - 1) * $per_page;
+        $users = User::skip($skip)->take($per_page)->get();
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        try {
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => bcrypt($validatedData['password']),
+            ]);
+
+            return response()->json($user, 201);
+        } catch (\Exception $ex) {
+            return response()->json(['error' => 'Falha ao criar usuário!'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            return response()->json($user, 200);
+        } catch (\Exception $ex) {
+            return response()->json(['error' => 'Falha ao buscar usuário!'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, string $id)
+    {
+        $validatedData = $request->validated();
+        try {
+            $user = User::findOrFail($id);
+            if(!$user){
+                return response()->json(['error' => 'Usuário não encontrado!'], 404);
+            }
+            $user->update($validatedData);
+            return response()->json($user, 200);
+        } catch (\Exception $ex) {
+            return response()->json(['error' => 'Falha ao atualizar usuário!'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->json(['message' => 'Usuário deletado com sucesso!'], 204);
+        } catch (\Exception $ex) {
+            return response()->json(['error' => 'Falha ao deletar usuário!'], 500);
+        }
+    }
+}
